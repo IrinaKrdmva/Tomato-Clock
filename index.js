4a37323ae971981f1f4fd02f5eba99ede6f62b45
Irina Karadimova <irinamaple84@gmail.com>
2024-02-27 19:39:12 +0000

    Update html

---
+++ index.js
@@ -0,0 +1,9 @@
+const controlBtn = document.querySelector(".control-btn");
+const workTimer = document.querySelector("#workTimer");
+const breakTimer = document.querySelector("#breakTimer");
+const addTimeBtn = document.querySelector("#addTimeBtn");
+const arm = document.querySelector(".arm");
+const circle = document.querySelector("#circleTimer");
+const workSeconds = 10;
+const breakSeconds = 10;
+const 
